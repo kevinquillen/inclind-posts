ecd906bdf29138de124fc8051b1b63a0ac0251d7
Kevin Quillen <kevin@inclind.com>
2014-09-21 15:34:22 -0400

    Adding convert script

---
+++ convert.rb
@@ -0,0 +1,33 @@
+require 'csv'
+
+posts = []
+rows = CSV.read(File.path("posts.csv"))
+rows.shift
+
+rows.each do |row|
+  page_title = row[1].strip
+  created = row[2]
+  body = row[3]
+  tags = row[4]
+
+  title = page_title.downcase
+  title.gsub!(/[^0-9a-z ]/i, '')
+  title.gsub(' ', '-')
+
+  body.gsub!('<p>', '')
+  body.gsub!('</p>', "\n\n")
+  body.gsub!(/\s+/, ' ')
+
+  filename = created + '-' + title + '.markdown'
+
+  File.open('posts/' + filename, 'w+') { |f|
+    f.write("---\n")
+    f.write("layout: journal\n")
+    f.write("title: " + page_title + "\n")
+    f.write("categories: \n")
+    f.write("show_chat: true\n")
+    f.write("show_contacts: false\n")
+    f.write("---\n\n")
+    f.write(body.strip)
+  }
+end
